3a79551f84888ede19167fe28f1302a83bc5f3d4
Nikita Pedorich <pedorich.n@gmail.com>
2023-04-13 18:32:19 +0900

    [vim] Use PName instead of Name

--- packages/vim-tomorrow-theme/default.nix
+++ packages/vim-tomorrow-theme/default.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, inputs }:
 pkgs.vimUtils.buildVimPluginFrom2Nix {
-  name = "tomorrow-theme";
+  pname = "tomorrow-theme";
   version = inputs.tomorrow-theme-source.shortRev;
   src = "${inputs.tomorrow-theme-source}/vim";
 }
