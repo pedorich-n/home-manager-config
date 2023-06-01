e7e80ac2d01b490933ea77a3671416690d4381d1
Nikita Pedorich <pedorich.n@gmail.com>
2023-06-01 20:19:52 +0900

    [flake] Switch stable channel to 23.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
 
     flake-utils.url = "github:numtide/flake-utils";
 
