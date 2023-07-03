1c0889ebf40bf5cbc89acf5ac9ca5e54d49144ca
Nikita Pedorich <pedorich.n@gmail.com>
2023-07-03 22:50:47 +0900

    [overlays] Remove copilot

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,8 +6,6 @@ inputs: _: prev:
     tomorrow-theme = prev.callPackage ../packages/vim-tomorrow-theme { inherit (inputs) tomorrow-theme-source; };
   };
 
-  github-copilot-intellij-agent = prev.callPackage ../packages/intellij-copilot { };
-
   python3Packages = prev.python3Packages // {
     cqlsh = prev.callPackage ../packages/cqlsh { inherit (inputs) cqlsh-source; };
     hmd = prev.callPackage ../packages/hmd { };
