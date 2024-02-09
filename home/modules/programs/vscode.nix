34a721e9fd42ae5311f0da79d8833c1503cf8dfb
Nikita Pedorich <pedorich.n@gmail.com>
2024-02-09 10:08:38 +0900

    [vscode] Add ruff; Reorganize settings.

--- home/modules/programs/vscode.nix
+++ home/modules/programs/vscode.nix
@@ -33,14 +33,14 @@ in
       space-ocean-kit-refined.space-ocean-kit-refined
 
       # Languages
+      charliermarsh.ruff
       jnoortheen.nix-ide
       mkhl.shfmt
-      ms-python.isort
       rust-lang.rust-analyzer
       scala-lang.scala
       scalameta.metals
-      tamasfe.even-better-toml
       skellock.just
+      tamasfe.even-better-toml
 
       # Behavior
       alefragnani.bookmarks
@@ -112,91 +112,132 @@ in
       }
     ] ++ keymapDisableOpenTabAtIndex;
 
-    userSettings = {
-      "editor.accessibilitySupport" = "off";
-      "editor.cursorSurroundingLines" = 10;
-      "editor.detectIndentation" = true;
-      "editor.fontFamily" = "FiraCode Nerd Font";
-      "editor.fontLigatures" = true;
-      "editor.fontSize" = 14;
-      "editor.formatOnPaste" = false;
-      "editor.formatOnSave" = true;
-      "editor.formatOnSaveMode" = "file";
-      "editor.formatOnType" = true;
-      "editor.indentSize" = 2;
-      "editor.inlineSuggest.enabled" = true;
-      "editor.insertSpaces" = true;
-      "editor.minimap.enabled" = false;
-      "editor.semanticHighlighting.enabled" = true;
-      "editor.stickyScroll.enabled" = true;
-      "editor.stickyScroll.maxLineCount" = 10;
-      "editor.tabSize" = 2;
-      "editor.useTabStops" = false;
-      "extensions.autoUpdate" = false;
-      "evenBetterToml.formatter.arrayAutoCollapse" = true;
-      "evenBetterToml.formatter.columnWidth" = 150;
-      "files.autoSave" = "afterDelay";
-      "files.encoding" = "utf8";
-      "files.eol" = "\n";
-      "files.insertFinalNewline" = true;
-      "files.trimFinalNewlines" = true;
-      "files.trimTrailingWhitespace" = true;
-      "files.watcherExclude" = watcherExclude;
-      "git.autofetch" = false;
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.serverSettings" = {
-        "nil" = {
-          "formatting" = {
-            "command" = [ "nixpkgs-fmt" ];
+    userSettings =
+      let
+        editorSettings = {
+          "editor.accessibilitySupport" = "off";
+          "editor.cursorSurroundingLines" = 10;
+          "editor.detectIndentation" = true;
+          "editor.fontFamily" = "FiraCode Nerd Font";
+          "editor.fontLigatures" = true;
+          "editor.fontSize" = 14;
+          "editor.formatOnPaste" = false;
+          "editor.formatOnSave" = true;
+          "editor.formatOnSaveMode" = "file";
+          "editor.formatOnType" = true;
+          "editor.indentSize" = 2;
+          "editor.inlineSuggest.enabled" = true;
+          "editor.insertSpaces" = true;
+          "editor.minimap.enabled" = false;
+          "editor.semanticHighlighting.enabled" = true;
+          "editor.stickyScroll.enabled" = true;
+          "editor.stickyScroll.maxLineCount" = 10;
+          "editor.tabSize" = 2;
+          "editor.useTabStops" = false;
+        };
+
+        filesSettings = {
+          "files.autoSave" = "afterDelay";
+          "files.encoding" = "utf8";
+          "files.eol" = "\n";
+          "files.insertFinalNewline" = true;
+          "files.trimFinalNewlines" = true;
+          "files.trimTrailingWhitespace" = true;
+          "files.watcherExclude" = watcherExclude;
+        };
+
+        nixSettings = {
+          "nix.enableLanguageServer" = true;
+          "nix.serverPath" = "nil";
+          "nix.serverSettings" = {
+            "nil" = {
+              "formatting" = {
+                "command" = [ "nixpkgs-fmt" ];
+              };
+            };
           };
         };
-      };
-      "metals.enableSemanticHighlighting" = true;
-      "search.useIgnoreFiles" = true;
-      "search.useGlobalIgnoreFiles" = true;
-      "presentation-mode.active" = {
-        "commands" = [ "workbench.action.closeSidebar" ];
-        "editor.fontSize" = 18;
-        "editor.minimap.enabled" = false;
-        "workbench.activityBar.visible" = false;
-        "workbench.statusBar.visible" = false;
-        "window.zoomLevel" = 2;
-      };
-      "presentation-mode.configBackup" = {
-        "editor.fontSize" = "undefined";
-        "editor.minimap.enabled" = "undefined";
-        "workbench.activityBar.visible" = "undefined";
-        "workbench.statusBar.visible" = "undefined";
-        "window.zoomLevel" = "undefined";
-      };
-      "black-formatter.args" = [ "--line-length=140" ];
-      "terminal.integrated.fontFamily" = "FiraCode Nerd Font";
-      "vim.foldfix" = true;
-      "vim.normalModeKeyBindingsNonRecursive" = [
-        {
-          "before" = [ "u" ];
-          "commands" = [ "undo" ];
-        }
-        {
-          "before" = [ "C-r" ];
-          "commands" = [ "redo" ];
-        }
-        {
-          "before" = [ "<Space>" "s" ];
-          "commands" = [ "workbench.action.gotoSymbol" ];
-        }
-        {
-          "before" = [ "<Space>" "r" ];
-          "commands" = [ "editor.action.rename" ];
-        }
-      ];
-      "vim.useSystemClipboard" = true;
-      "window.newWindowDimensions" = "maximized";
-      "window.openFoldersInNewWindow" = "on";
-      "window.zoomLevel" = 0.6;
-      "workbench.colorTheme" = "Dark Low Contrast Fire";
-      "workbench.editor.wrapTabs" = true;
-    };
+
+        presentationModeSettings = {
+          "presentation-mode.active" = {
+            "commands" = [ "workbench.action.closeSidebar" ];
+            "editor.fontSize" = 18;
+            "editor.minimap.enabled" = false;
+            "workbench.activityBar.visible" = false;
+            "workbench.statusBar.visible" = false;
+            "window.zoomLevel" = 2;
+          };
+          "presentation-mode.configBackup" = {
+            "editor.fontSize" = "undefined";
+            "editor.minimap.enabled" = "undefined";
+            "workbench.activityBar.visible" = "undefined";
+            "workbench.statusBar.visible" = "undefined";
+            "window.zoomLevel" = "undefined";
+          };
+        };
+
+        pythonSettings = {
+          "ruff.lint.args" = [ "--line-length=140" ];
+          "ruff.format.args" = [ "--line-length=140" ];
+          "[python]" = {
+            "editor.defaultFormatter" = "charliermarsh.ruff";
+          };
+        };
+
+        scalaSettings = {
+          "metals.enableSemanticHighlighting" = true;
+        };
+
+        tomlSettings = {
+          "evenBetterToml.formatter.arrayAutoCollapse" = true;
+          "evenBetterToml.formatter.columnWidth" = 150;
+        };
+
+        vimSettings = {
+          "vim.foldfix" = true;
+          "vim.normalModeKeyBindingsNonRecursive" = [
+            {
+              "before" = [ "u" ];
+              "commands" = [ "undo" ];
+            }
+            {
+              "before" = [ "C-r" ];
+              "commands" = [ "redo" ];
+            }
+            {
+              "before" = [ "<Space>" "s" ];
+              "commands" = [ "workbench.action.gotoSymbol" ];
+            }
+            {
+              "before" = [ "<Space>" "r" ];
+              "commands" = [ "editor.action.rename" ];
+            }
+          ];
+          "vim.useSystemClipboard" = true;
+        };
+
+        vscodeMiscSettings = {
+          "extensions.autoUpdate" = false;
+          "git.autofetch" = false;
+          "search.useIgnoreFiles" = true;
+          "search.useGlobalIgnoreFiles" = true;
+          "terminal.integrated.fontFamily" = "FiraCode Nerd Font";
+          "window.newWindowDimensions" = "maximized";
+          "window.openFoldersInNewWindow" = "on";
+          "window.zoomLevel" = 0.6;
+          "workbench.colorTheme" = "Dark Low Contrast Fire";
+          "workbench.editor.wrapTabs" = true;
+        };
+
+      in
+      editorSettings //
+      filesSettings //
+      nixSettings //
+      presentationModeSettings //
+      pythonSettings //
+      scalaSettings //
+      tomlSettings //
+      vimSettings //
+      vscodeMiscSettings;
   };
 }
