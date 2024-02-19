f01759e5c2b8401d67100bb6661c86f5563daf09
Nikita Pedorich <pedorich.n@gmail.com>
2024-02-19 09:29:00 +0900

    [tealdeer] Update HM; Update tldr settings

--- home/modules/programs/tealdeer.nix
+++ home/modules/programs/tealdeer.nix
@@ -1,6 +1,11 @@
 {
   programs.tealdeer = {
     settings = {
+      updates = {
+        auto_update = true;
+        auto_update_interval_hours = 168; # 1 week
+      };
+
       display = {
         use_pager = false;
         compact = true;
