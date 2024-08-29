979160e29c6419d757f789a200ff5949cfb278df
Mykyta Pedorich <mykyta.pedorich@paidy.com>
2024-08-29 10:46:12 +0900

    [bootstrap] Add flakes support

--- packages/bootstrap/default.nix
+++ packages/bootstrap/default.nix
@@ -11,13 +11,13 @@ pkgs.writeShellApplication {
     FLAKE_PATH="$HOME/.config.nix"
 
     config_name=''${1:-${configName}}
-  
+
     if [ -d "$FLAKE_PATH" ]; then
         echo "Target folder already exists! Aborting"
         exit 1
     else
         echo "Target folder does not exist. Cloning the repository..."
-        
+
         if [ "$(git clone "$REPO_URL" "$FLAKE_PATH")" ]; then
             echo "Repository cloned successfully into $FLAKE_PATH."
         else
@@ -28,6 +28,7 @@ pkgs.writeShellApplication {
 
     echo "Running home-manager switch..."
     export HOME_MANAGER_BACKUP_EXT="backup"
-    home-manager switch --flake "$FLAKE_PATH#$config_name"
+    export NIXPKGS_ALLOW_UNFREE=1
+    home-manager switch --extra-experimental-features "nix-command flakes" --flake "$FLAKE_PATH#$config_name"
   '';
 }
