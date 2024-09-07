e9a8d2f498d0e61352728b9aaa907e570448dacc
Nikita Pedorich <pedorich.n@gmail.com>
2024-09-08 01:14:18 +0900

    WIP. replace loader; Use new loader for homeModules

--- flake-parts/homeModules.nix
+++ flake-parts/homeModules.nix
@@ -2,7 +2,7 @@
   flake = {
     homeModules = {
       common = { imports = [ ../home/configurations/common.nix ]; };
-      sharedModules = { imports = flake.lib.loaders.listNixFiles { src = ../home/modules; }; };
+      sharedModules = { imports = flake.lib.loaders.listFilesRecursivelly { src = ../home/modules; }; };
     };
   };
 }
