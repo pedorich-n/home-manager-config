e5db9ca47242e61c7bc3934c1493dce9bbb4b079
Nikita Pedorich <pedorich.n@gmail.com>
2024-09-08 01:42:42 +0900

    Migrate shells

--- flake-parts/shells.nix
+++ flake-parts/shells.nix
@@ -1,29 +1,15 @@
-{ inputs, ... }:
+{ flake, ... }:
 {
-  perSystem = { lib, pkgs, ... }:
-    let
-      # Flattens an attrset of arbitrary depth, where the values are either derivations or attrsets into a flat attrset of derivations.
-      flattenDerivations = derivations: lib.foldl'
-        (acc: attrset:
-          if lib.isDerivation attrset.value then
-            acc // { ${attrset.name} = attrset.value; }
-          else if lib.isAttrs attrset.value then
-            acc // (flattenDerivations attrset.value)
-          else
-            lib.trace attrset (abort "Unknown type in attrset")
-        )
-        { }
-        (lib.attrsToList derivations);
-    in
-    {
-      devShells = flattenDerivations (
-        inputs.haumea.lib.load {
-          src = ../shells;
-          loader = inputs.haumea.lib.loaders.default;
-          inputs = {
-            inherit pkgs;
-          };
-        }
-      );
-    };
+  perSystem = { pkgs, lib, ... }: {
+    /* 
+      Loads all shells from ../shells folder.
+      Expected format of a shell: attrset containg derivations in a file
+      So one file can contain multiple shells
+    */
+    devShells = lib.pipe ../shells [
+      (src: flake.lib.loaders.listFilesRecursivelly { inherit src; })
+      (builtins.map (shell: import shell { inherit pkgs lib; }))
+      (lib.foldl' (acc: attrs: lib.recursiveUpdate acc attrs) { })
+    ];
+  };
 }
