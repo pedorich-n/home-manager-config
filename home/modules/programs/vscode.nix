6170633e0c8347692c8c9c2f7fdc8da4df0c0a63
Mykyta Pedorich <mykyta.pedorich@paidy.com>
2025-02-26 12:50:02 +0900

    Update flake.lock

--- home/modules/programs/vscode.nix
+++ home/modules/programs/vscode.nix
@@ -20,254 +20,258 @@ in
   programs.vscode = {
     package = pkgs.vscode;
 
-    extensions = (with pkgs.vscode-extensions; [
-      # Using those from nixpkgs, because they require extra setup which is not provivided in vscode-marketplace
-      github.copilot
-      jebbs.plantuml
-      hashicorp.terraform
-    ]) ++ (with pkgs.vscode-marketplace; [
-      # Themes
-      evgeniypetukhov.dark-low-contrast
-      dustinsanders.an-old-hope-theme-vscode
-      pawelgrzybek.gatito-theme
-      space-ocean-kit-refined.space-ocean-kit-refined
+    profiles.default = {
 
-      # Languages
-      arrterian.nix-env-selector
-      charliermarsh.ruff
-      jnoortheen.nix-ide
-      kdl-org.kdl
-      mads-hartmann.bash-ide-vscode
-      mkhl.shfmt
-      rust-lang.rust-analyzer
-      scala-lang.scala
-      scalameta.metals
-      skellock.just
-      tamasfe.even-better-toml
-      redhat.vscode-yaml
-      ms-azuretools.vscode-docker
-      ms-python.python
-      ms-python.vscode-pylance
-      wholroyd.jinja
+      extensions = (with pkgs.vscode-extensions; [
+        # Using those from nixpkgs, because they require extra setup which is not provivided in vscode-marketplace
+        github.copilot
+        jebbs.plantuml
+        hashicorp.terraform
+      ]) ++ (with pkgs.vscode-marketplace; [
+        # Themes
+        evgeniypetukhov.dark-low-contrast
+        dustinsanders.an-old-hope-theme-vscode
+        pawelgrzybek.gatito-theme
+        space-ocean-kit-refined.space-ocean-kit-refined
 
-      # Behavior
-      ast-grep.ast-grep-vscode
-      alefragnani.bookmarks
-      cs50.vscode-presentation-mode
-      donjayamanne.githistory
-      fabiospampinato.vscode-open-in-github
-      gruntfuggly.todo-tree
-      k--kato.intellij-idea-keybindings
-      natqe.reload
-      shd101wyy.markdown-preview-enhanced
-      vscodevim.vim
-      wmaurer.vscode-jumpy
-      editorconfig.editorconfig
-      esbenp.prettier-vscode
-      exodiusstudios.comment-anchors
-    ]);
+        # Languages
+        arrterian.nix-env-selector
+        charliermarsh.ruff
+        jnoortheen.nix-ide
+        kdl-org.kdl
+        mads-hartmann.bash-ide-vscode
+        mkhl.shfmt
+        rust-lang.rust-analyzer
+        scala-lang.scala
+        scalameta.metals
+        skellock.just
+        tamasfe.even-better-toml
+        redhat.vscode-yaml
+        ms-azuretools.vscode-docker
+        ms-python.python
+        ms-python.vscode-pylance
+        wholroyd.jinja
 
-    keybindings = [
-      {
-        key = "ctrl+`";
-        command = "workbench.action.terminal.focus";
-      }
-      {
-        key = "ctrl+`";
-        command = "workbench.action.focusActiveEditorGroup";
-        when = "terminalFocus";
-      }
-      {
-        key = "shift+alt+l";
-        command = "editor.action.formatDocument";
-        when = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
-      }
-      {
-        key = "shift+alt+f";
-        command = "-editor.action.formatDocument";
-        when = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
-      }
-      {
-        key = "shift+alt+up";
-        command = "-extension.vim_cmd+alt+up";
-        when = "editorTextFocus && vim.active && !inDebugRepl";
-      }
-      {
-        key = "shift+alt+down";
-        command = "-extension.vim_cmd+alt+down";
-        when = "editorTextFocus && vim.active && !inDebugRepl";
-      }
-      {
-        key = "alt+g";
-        command = "openInGitHub.openFile";
-        when = "editorTextFocus";
-      }
-      {
-        key = "alt+1";
-        command = "workbench.view.explorer";
-        when = "!view.workbench.explorer.fileView.visible";
-      }
-      {
-        key = "alt+1";
-        command = "workbench.action.toggleSidebarVisibility";
-        when = "view.workbench.explorer.fileView.visible";
-      }
-      {
-        key = "alt+2";
-        command = "workbench.view.extension.bookmarks";
-        when = "editorFocus";
-      }
-      {
-        key = "alt+2";
-        command = "workbench.action.toggleSidebarVisibility";
-        when = "!editorFocus";
-      }
-    ] ++ keymapDisableOpenTabAtIndex;
+        # Behavior
+        ast-grep.ast-grep-vscode
+        alefragnani.bookmarks
+        cs50.vscode-presentation-mode
+        donjayamanne.githistory
+        fabiospampinato.vscode-open-in-github
+        gruntfuggly.todo-tree
+        k--kato.intellij-idea-keybindings
+        natqe.reload
+        shd101wyy.markdown-preview-enhanced
+        vscodevim.vim
+        wmaurer.vscode-jumpy
+        editorconfig.editorconfig
+        esbenp.prettier-vscode
+        exodiusstudios.comment-anchors
+      ]);
 
-    userSettings =
-      let
-        editorSettings = {
-          "editor.accessibilitySupport" = "off";
-          "editor.cursorSurroundingLines" = 10;
-          "editor.detectIndentation" = true;
-          "editor.fontFamily" = "FiraCode Nerd Font";
-          "editor.fontLigatures" = true;
-          "editor.fontSize" = 14;
-          "editor.formatOnPaste" = false;
-          "editor.formatOnSave" = true;
-          "editor.formatOnSaveMode" = "file";
-          "editor.formatOnType" = true;
-          "editor.indentSize" = 2;
-          "editor.inlineSuggest.enabled" = true;
-          "editor.insertSpaces" = true;
-          "editor.minimap.enabled" = false;
-          "editor.semanticHighlighting.enabled" = true;
-          "editor.stickyScroll.enabled" = true;
-          "editor.stickyScroll.maxLineCount" = 10;
-          "editor.tabSize" = 2;
-          "editor.useTabStops" = false;
-        };
+      keybindings = [
+        {
+          key = "ctrl+`";
+          command = "workbench.action.terminal.focus";
+        }
+        {
+          key = "ctrl+`";
+          command = "workbench.action.focusActiveEditorGroup";
+          when = "terminalFocus";
+        }
+        {
+          key = "shift+alt+l";
+          command = "editor.action.formatDocument";
+          when = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
+        }
+        {
+          key = "shift+alt+f";
+          command = "-editor.action.formatDocument";
+          when = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
+        }
+        {
+          key = "shift+alt+up";
+          command = "-extension.vim_cmd+alt+up";
+          when = "editorTextFocus && vim.active && !inDebugRepl";
+        }
+        {
+          key = "shift+alt+down";
+          command = "-extension.vim_cmd+alt+down";
+          when = "editorTextFocus && vim.active && !inDebugRepl";
+        }
+        {
+          key = "alt+g";
+          command = "openInGitHub.openFile";
+          when = "editorTextFocus";
+        }
+        {
+          key = "alt+1";
+          command = "workbench.view.explorer";
+          when = "!view.workbench.explorer.fileView.visible";
+        }
+        {
+          key = "alt+1";
+          command = "workbench.action.toggleSidebarVisibility";
+          when = "view.workbench.explorer.fileView.visible";
+        }
+        {
+          key = "alt+2";
+          command = "workbench.view.extension.bookmarks";
+          when = "editorFocus";
+        }
+        {
+          key = "alt+2";
+          command = "workbench.action.toggleSidebarVisibility";
+          when = "!editorFocus";
+        }
+      ] ++ keymapDisableOpenTabAtIndex;
 
-        filesSettings = {
-          "files.autoSave" = "afterDelay";
-          "files.encoding" = "utf8";
-          "files.eol" = "\n";
-          "files.insertFinalNewline" = true;
-          "files.trimFinalNewlines" = true;
-          "files.trimTrailingWhitespace" = true;
-          "files.watcherExclude" = watcherExclude;
-          "files.associations" = {
-            "*.uml" = "plantuml";
+      userSettings =
+        let
+          editorSettings = {
+            "editor.accessibilitySupport" = "off";
+            "editor.cursorSurroundingLines" = 10;
+            "editor.detectIndentation" = true;
+            "editor.fontFamily" = "FiraCode Nerd Font";
+            "editor.fontLigatures" = true;
+            "editor.fontSize" = 14;
+            "editor.formatOnPaste" = false;
+            "editor.formatOnSave" = true;
+            "editor.formatOnSaveMode" = "file";
+            "editor.formatOnType" = true;
+            "editor.indentSize" = 2;
+            "editor.inlineSuggest.enabled" = true;
+            "editor.insertSpaces" = true;
+            "editor.minimap.enabled" = false;
+            "editor.semanticHighlighting.enabled" = true;
+            "editor.stickyScroll.enabled" = true;
+            "editor.stickyScroll.maxLineCount" = 10;
+            "editor.tabSize" = 2;
+            "editor.useTabStops" = false;
           };
-        };
 
-        nixSettings = {
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "nil";
-          "nix.serverSettings" = {
-            "nil" = {
-              "formatting" = {
-                "command" = [ "nixpkgs-fmt" ];
+          filesSettings = {
+            "files.autoSave" = "afterDelay";
+            "files.encoding" = "utf8";
+            "files.eol" = "\n";
+            "files.insertFinalNewline" = true;
+            "files.trimFinalNewlines" = true;
+            "files.trimTrailingWhitespace" = true;
+            "files.watcherExclude" = watcherExclude;
+            "files.associations" = {
+              "*.uml" = "plantuml";
+            };
+          };
+
+          nixSettings = {
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nil";
+            "nix.serverSettings" = {
+              "nil" = {
+                "formatting" = {
+                  "command" = [ "nixpkgs-fmt" ];
+                };
               };
             };
           };
-        };
 
-        presentationModeSettings = {
-          "presentation-mode.active" = {
-            "commands" = [ "workbench.action.closeSidebar" ];
-            "editor.fontSize" = 18;
-            "editor.minimap.enabled" = false;
-            "workbench.activityBar.visible" = false;
-            "workbench.statusBar.visible" = false;
-            "window.zoomLevel" = 2;
+          presentationModeSettings = {
+            "presentation-mode.active" = {
+              "commands" = [ "workbench.action.closeSidebar" ];
+              "editor.fontSize" = 18;
+              "editor.minimap.enabled" = false;
+              "workbench.activityBar.visible" = false;
+              "workbench.statusBar.visible" = false;
+              "window.zoomLevel" = 2;
+            };
+            "presentation-mode.configBackup" = {
+              "editor.fontSize" = "undefined";
+              "editor.minimap.enabled" = "undefined";
+              "workbench.activityBar.visible" = "undefined";
+              "workbench.statusBar.visible" = "undefined";
+              "window.zoomLevel" = "undefined";
+            };
           };
-          "presentation-mode.configBackup" = {
-            "editor.fontSize" = "undefined";
-            "editor.minimap.enabled" = "undefined";
-            "workbench.activityBar.visible" = "undefined";
-            "workbench.statusBar.visible" = "undefined";
-            "window.zoomLevel" = "undefined";
+
+          pythonSettings = {
+            "ruff.lineLength" = 140;
+            "[python]" = {
+              "editor.defaultFormatter" = "charliermarsh.ruff";
+            };
+          } // (lib.optionalAttrs config.custom.programs.python.enable {
+            "python.defaultInterpreterPath" = lib.getExe config.custom.programs.python.resultEnv;
+          });
+
+          scalaSettings = {
+            "metals.enableSemanticHighlighting" = true;
           };
-        };
 
-        pythonSettings = {
-          "ruff.lineLength" = 140;
-          "[python]" = {
-            "editor.defaultFormatter" = "charliermarsh.ruff";
+          tomlSettings = {
+            "evenBetterToml.formatter.arrayAutoCollapse" = true;
+            "evenBetterToml.formatter.columnWidth" = 150;
           };
-        } // (lib.optionalAttrs config.custom.programs.python.enable {
-          "python.defaultInterpreterPath" = lib.getExe config.custom.programs.python.resultEnv;
-        });
 
-        scalaSettings = {
-          "metals.enableSemanticHighlighting" = true;
-        };
-
-        tomlSettings = {
-          "evenBetterToml.formatter.arrayAutoCollapse" = true;
-          "evenBetterToml.formatter.columnWidth" = 150;
-        };
-
-        vimSettings = {
-          "vim.foldfix" = true;
-          "vim.normalModeKeyBindingsNonRecursive" = [
-            {
-              "before" = [ "u" ];
-              "commands" = [ "undo" ];
-            }
-            {
-              "before" = [ "C-r" ];
-              "commands" = [ "redo" ];
-            }
-            {
-              "before" = [ "<Space>" "s" ];
-              "commands" = [ "workbench.action.gotoSymbol" ];
-            }
-            {
-              "before" = [ "<Space>" "r" ];
-              "commands" = [ "editor.action.rename" ];
-            }
-          ];
-          "vim.useSystemClipboard" = true;
-        };
-
-        vscodeMiscSettings = {
-          "extensions.autoUpdate" = false;
-          "git.autofetch" = false;
-          "search.useIgnoreFiles" = true;
-          "search.useGlobalIgnoreFiles" = true;
-          "terminal.integrated.fontFamily" = "FiraCode Nerd Font";
-          "window.newWindowDimensions" = "maximized";
-          "window.openFoldersInNewWindow" = "on";
-          "window.zoomLevel" = 0.6;
-          "workbench.colorTheme" = "Dark Low Contrast Fire";
-          "workbench.editor.wrapTabs" = true;
-        };
-
-        yamlSettings = {
-          "[yaml]" = {
-            "editor.defaultFormatter" = "redhat.vscode-yaml";
+          vimSettings = {
+            "vim.foldfix" = true;
+            "vim.normalModeKeyBindingsNonRecursive" = [
+              {
+                "before" = [ "u" ];
+                "commands" = [ "undo" ];
+              }
+              {
+                "before" = [ "C-r" ];
+                "commands" = [ "redo" ];
+              }
+              {
+                "before" = [ "<Space>" "s" ];
+                "commands" = [ "workbench.action.gotoSymbol" ];
+              }
+              {
+                "before" = [ "<Space>" "r" ];
+                "commands" = [ "editor.action.rename" ];
+              }
+            ];
+            "vim.useSystemClipboard" = true;
           };
-        };
 
-        jsonSettings = {
-          "[json]" = {
-            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+          vscodeMiscSettings = {
+            "extensions.autoUpdate" = false;
+            "git.autofetch" = false;
+            "search.useIgnoreFiles" = true;
+            "search.useGlobalIgnoreFiles" = true;
+            "terminal.integrated.fontFamily" = "FiraCode Nerd Font";
+            "window.newWindowDimensions" = "maximized";
+            "window.openFoldersInNewWindow" = "on";
+            "window.zoomLevel" = 0.6;
+            "workbench.colorTheme" = "Dark Low Contrast Fire";
+            "workbench.editor.wrapTabs" = true;
           };
-        };
 
-      in
-      editorSettings //
-      filesSettings //
-      nixSettings //
-      presentationModeSettings //
-      pythonSettings //
-      scalaSettings //
-      tomlSettings //
-      vimSettings //
-      vscodeMiscSettings //
-      yamlSettings //
-      jsonSettings;
+          yamlSettings = {
+            "[yaml]" = {
+              "editor.defaultFormatter" = "redhat.vscode-yaml";
+            };
+          };
+
+          jsonSettings = {
+            "[json]" = {
+              "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+          };
+
+        in
+        editorSettings //
+        filesSettings //
+        nixSettings //
+        presentationModeSettings //
+        pythonSettings //
+        scalaSettings //
+        tomlSettings //
+        vimSettings //
+        vscodeMiscSettings //
+        yamlSettings //
+        jsonSettings;
+    };
   };
+
 }
