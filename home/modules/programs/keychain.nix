9a28bfb6532841d3bc8a386d3d04beb3cac372a2
Nikita Pedorich <pedorich.n@gmail.com>
2025-06-03 23:06:38 +0900

    keychain: remove deprecated options

--- home/modules/programs/keychain.nix
+++ home/modules/programs/keychain.nix
@@ -1,9 +1,7 @@
-{ config, lib, ... }: {
+{ lib, ... }: {
   programs.keychain = {
     enableBashIntegration = lib.mkDefault true;
     enableZshIntegration = lib.mkDefault true;
     enableXsessionIntegration = lib.mkDefault true;
-
-    agents = [ "ssh" ] ++ lib.optional config.services.gpg-agent.enable "gpg";
   };
 }
