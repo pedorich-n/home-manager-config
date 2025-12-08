a93a3b457c28145383a4b1efd4c05702b78a59f2
Mykyta Pedorich <mykyta.pedorich@paidy.com>
2025-12-08 09:24:13 +0900

    Update cqlsh

--- packages/cqlsh/default.nix
+++ packages/cqlsh/default.nix
@@ -1,10 +1,17 @@
 { pkgs, ... }:
+let
+  python = pkgs.python3.withPackages (pp: [
+    pp.pytz
+    pp.tzlocal
+  ]);
+
+in
 pkgs.symlinkJoin {
   name = "cqlsh";
   paths = [ pkgs.cassandra ];
   nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
 
   postBuild = ''
-    wrapProgram $out/bin/cqlsh --prefix PATH : ${pkgs.python27}/bin
+    wrapProgram $out/bin/cqlsh --prefix PATH : ${python}/bin
   '';
 }
