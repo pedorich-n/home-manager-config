5c220cd90ab8a9376cc1c69239871970d0c5d437
Nikita Pedorich <pedorich.n@gmail.com>
2026-03-30 00:26:13 +0900

    Add zsh-tab-title

--- flake-parts/packages.nix
+++ flake-parts/packages.nix
@@ -8,6 +8,8 @@
         wsl-1password-cli = pkgs.callPackage ../packages/wsl-1password-cli { };
 
         hmd = pkgs.callPackage ../packages/hmd { };
+
+        zsh-tab-title = pkgs.callPackage ../packages/zsh-tab-title { };
       };
     };
 }
