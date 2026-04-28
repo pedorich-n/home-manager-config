c843150b1962d4917fdfd278b5f97878c589e39b
Nikita Pedorich <pedorich.n@gmail.com>
2026-04-28 21:41:48 +0900

    More renames

--- flake-parts/apps.nix
+++ flake-parts/apps.nix
@@ -18,14 +18,16 @@
       mkBootstrap = name: {
         name = "bootstrap-${name}";
         value = {
-          program = tools.bootstrap.override { configName = name; };
+          program = tools.hm-bootstrap.override { configName = name; };
         };
       };
+
+      bootstrapApps = lib.mapAttrs' (name: _: mkBootstrap name) flake.homeConfigurations;
     in
     {
       apps = {
         install-nix-selinux.program = tools.nix-selinux.install;
       }
-      // (lib.mapAttrs' (name: _: mkBootstrap name) flake.homeConfigurations);
+      // bootstrapApps;
     };
 }
