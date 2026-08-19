90a1059a98ec881b2389e4eb068b778d86ec3770
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-19 21:08:04 +0900

    Move linuxWork to new structure

---
+++ home/configurations/linuxWork/custom.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  ...
+}:
+{
+  custom = {
+    aliases.hms.configName = "linuxWork";
+    dotfiles.enable = true;
+    runtimes = {
+      enable = true;
+      java = [
+        pkgs.jdk11
+        pkgs.jdk8
+      ];
+    };
+
+    programs = {
+      aws-sso-cli.enable = true;
+      flameshot.enable = true;
+      gnome.enable = true;
+      rust.enable = true;
+      scala.enable = true;
+      gpg.enable = true;
+      python = {
+        enable = true;
+        extraPackages =
+          pythonPkgs: with pythonPkgs; [
+            requests
+            pandas
+          ];
+
+        uv.enable = true;
+      };
+    };
+  };
+}
