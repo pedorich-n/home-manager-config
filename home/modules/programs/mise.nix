b82fc4bdf86b0e10cdd21a918601c5afebf11ec7
Nikita Pedorich <pedorich.n@gmail.com>
2026-08-23 14:37:46 +0900

    Mise: enable mutable config

--- home/modules/programs/mise.nix
+++ home/modules/programs/mise.nix
@@ -1,11 +1,14 @@
 {
-  programs.mise.globalConfig = {
-    settings = {
-      # Enables support for .tool-versions, .terraform-version, .nvmrc, etc. files
-      idiomatic_version_file_enable_tools = [
-        "terraform"
-        "node"
-      ];
+  programs.mise = {
+    enableMutableConfig = true;
+    globalConfig = {
+      settings = {
+        # Enables support for .tool-versions, .terraform-version, .nvmrc, etc. files
+        idiomatic_version_file_enable_tools = [
+          "terraform"
+          "node"
+        ];
+      };
     };
   };
 }
