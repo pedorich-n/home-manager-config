7d7280f6588b86f80d5fc381fc1f097e0d0f0cdd
Nikita Pedorich <pedorich.n@gmail.com>
2026-09-01 00:44:38 +0900

    Add Searxng MCP

--- home/configurations/desktopPersonal/programs/mcp.nix
+++ home/configurations/desktopPersonal/programs/mcp.nix
@@ -1,3 +1,15 @@
 {
-  programs.mcp.enable = true;
+  config,
+  ...
+}:
+{
+  programs.mcp = {
+    enable = true;
+
+    servers = {
+      searxng = {
+        inherit (config.custom.secrets.plaintext.mcp.searxng) url;
+      };
+    };
+  };
 }
