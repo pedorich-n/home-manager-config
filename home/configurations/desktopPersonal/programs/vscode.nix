d30b681fd2e111a1ebbcc7e42380075189a44d59
Nikita Pedorich <pedorich.n@gmail.com>
2026-09-03 09:17:30 +0900

    vscode: drop Continue

--- home/configurations/desktopPersonal/programs/vscode.nix
+++ home/configurations/desktopPersonal/programs/vscode.nix
@@ -1,23 +1,5 @@
 {
-  config,
-  pkgs,
-  ...
-}:
-{
   programs.vscode = {
     enable = true;
-    profiles.default = {
-      extensions = [
-        pkgs.vscode-extensions.continue.continue
-      ];
-
-      userSettings = {
-        "yaml.schemas" = {
-          "file://${config.home.homeDirectory}/.vscode/extensions/Continue.continue/config-yaml-schema.json" = [
-            ".continue/**/*.yaml"
-          ];
-        };
-      };
-    };
   };
 }
